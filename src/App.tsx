98d6c59cfd545934ebd4f5c09cc2ea0dde5b5974
harryuan65 <harryuan.65@gmail.com>
2023-02-06 19:12:11 +0800

    feat: implement tab, tab item

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Guitar from './components/Guitar';
+import Tabs, { TabItem } from './components/Tabs';
 import Scale from './utils/ScaleFinder';
-
+import styles from './App.module.css';
 function App() {
   const defaultKey = 'C';
   const defaultScaleType = 'minor';
@@ -11,11 +12,27 @@ function App() {
 
   return (
     <div className="App">
-      <Guitar highlightScale={defaultScale.scaleNotesMap} />
-      <h2>
-        Displaying scale: {defaultKey} {defaultScaleType}
-      </h2>
-      <h3>Notes: {scaleNotes.join(', ')}</h3>
+      <main className={styles.main}>
+        <Guitar highlightScale={defaultScale.scaleNotesMap} />
+        <h2>
+          Displaying scale: {defaultKey} {defaultScaleType}
+        </h2>
+        <h3>Notes: {scaleNotes.join(', ')}</h3>
+        <Tabs>
+          <TabItem title="Hello">Hello</TabItem>
+          <TabItem title="World">World</TabItem>
+          <TabItem title="天阿">
+            <div>
+              <button
+                style={{ cursor: 'pointer' }}
+                onClick={() => console.log('Hello!')}
+              >
+                按按看～
+              </button>
+            </div>
+          </TabItem>
+        </Tabs>
+      </main>
     </div>
   );
 }
---
+++ src/components/Tabs/index.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import styles from './styles.module.css';
+
+interface TabItemProps {
+  title: string;
+  active?: boolean;
+  children?: React.ReactElement | string | null;
+}
+
+interface TabsProps {
+  children: React.ReactElement<TabItemProps>[] | null;
+}
+
+const TabItem: React.FC<TabItemProps> = ({ title, children, active }) => {
+  return (
+    <div className={[styles.tabItem, active && styles.active].join(' ')}>
+      {children}
+    </div>
+  );
+};
+
+const Tabs: React.FC<TabsProps> = ({ children }: TabsProps) => {
+  const [activeIndex, setActive] = useState(0);
+  const updatedChildren = React.Children.map(children, (child, i) => {
+    if (React.isValidElement(child)) {
+      return React.cloneElement(child, {
+        active: activeIndex === i,
+      });
+    }
+    return child;
+  });
+  return (
+    <div>
+      <div>
+        {children?.map(({ props }, i) => (
+          <span
+            key={props.title}
+            className={[
+              styles.tabEntry,
+              activeIndex === i && styles.active,
+            ].join(' ')}
+            onClick={() => setActive(i)}
+          >
+            {props.title}
+          </span>
+        ))}
+      </div>
+      {updatedChildren}
+    </div>
+  );
+};
+export { TabItem };
+export default Tabs;
