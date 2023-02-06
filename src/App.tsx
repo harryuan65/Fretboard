2cf0e2a8549a783a27aa3b1fe8719710de3a802d
harryuan65 <harryuan.65@gmail.com>
2023-02-06 15:30:18 +0800

    feat: display current scale notes

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,19 @@ function App() {
   const defaultKey = 'C';
   const defaultScaleType = 'minor';
   const defaultScale = Scale.list(defaultKey, defaultScaleType);
-  console.log(defaultScale);
+  const scaleNotes = Object.entries(defaultScale)
+    .sort(([_noteA, ordInfoA], [_noteB, ordInfoB]) => {
+      return ordInfoA.ord - ordInfoB.ord;
+    })
+    .map(([note, _ord]) => note);
 
   return (
     <div className="App">
-      <h1>
-        Displaying scale: {defaultKey} {defaultScaleType}
-      </h1>
       <Guitar highlightScale={defaultScale} />
+      <h2>
+        Displaying scale: {defaultKey} {defaultScaleType}
+      </h2>
+      <h3>Notes: {scaleNotes.join(', ')}</h3>
     </div>
   );
 }
