14f1b4f5d6e8c81fd0da2ddff697950936d2e9ba
harryuan65 <harryuan.65@gmail.com>
2023-02-06 22:28:35 +0800

    refactor: organize useScales

--- src/hooks/useScales.tsx
+++ src/hooks/useScales.tsx
@@ -5,29 +5,24 @@ import Scale, { ScaleName } from '../utils/Scale';
 const useScales = () => {
   const [key, setKey] = useState<Note | null>();
   const [scaleType, setScaleType] = useState<ScaleName | null>();
+  let scaleNotes = null;
+  let scaleNotesMap = null;
+
   if (key && scaleType) {
     const defaultScale = new Scale(key, scaleType);
     defaultScale.calculateList();
-    const scaleNotes = defaultScale.scaleNotes;
-    const scaleNotesMap = defaultScale.scaleNotesMap;
-    return {
-      key,
-      setKey,
-      scaleType,
-      setScaleType,
-      scaleNotes,
-      scaleNotesMap,
-    };
-  } else {
-    return {
-      key,
-      setKey,
-      scaleType,
-      setScaleType,
-      scaleNotes: null,
-      scaleNotesMap: null,
-    };
+    scaleNotes = defaultScale.scaleNotes;
+    scaleNotesMap = defaultScale.scaleNotesMap;
   }
+
+  return {
+    key,
+    setKey,
+    scaleType,
+    setScaleType,
+    scaleNotes,
+    scaleNotesMap,
+  };
 };
 
 export default useScales;
