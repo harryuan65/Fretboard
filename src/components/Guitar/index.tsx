7db3ff975e24e831bf2e77a2ba4e635fe3cd7ab7
harryuan65 <harryuan.65@gmail.com>
2023-02-06 23:53:57 +0800

    feat: add fret number

--- src/components/Guitar/index.tsx
+++ src/components/Guitar/index.tsx
@@ -10,6 +10,21 @@ interface GuitarProps {
 
 const Guitar = ({ scaleMap }: GuitarProps) => {
   const strings = freq.strings as IFret[][];
+  let renderFretNumber = strings[0].map((_fret, i) => {
+    let fretNum = null;
+    switch (i + 1) {
+      case 3:
+      case 5:
+      case 7:
+      case 9:
+      case 12:
+      case 15:
+      case 17:
+        fretNum = i + 1;
+        break;
+    }
+    return <span className={styles.fretNum}>{fretNum}</span>;
+  });
   return (
     <div className={styles.guitar}>
       {strings.map((stringFrets, i) => (
@@ -19,6 +34,7 @@ const Guitar = ({ scaleMap }: GuitarProps) => {
           scaleMap={scaleMap}
         />
       ))}
+      {<div className={styles.fretNumWrap}>{renderFretNumber}</div>}
     </div>
   );
 };
