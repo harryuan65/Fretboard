e7847e37fcf939a3545b0262c0ebcf00ce24837d
harryuan65 <harryuan.65@gmail.com>
2023-02-06 10:49:59 +0800

    feat: list frets and play sound

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as freq from './freq.json';
+import styles from './App.module.css';
+
+interface StringProps {
+  freqs: number[];
+}
+
+const play = (freq: number, duration: number) => {
+  const context = new AudioContext();
+  const gainNode = context.createGain();
+  const oscillator = context.createOscillator();
+  oscillator.frequency.value = freq;
+  oscillator.connect(gainNode);
+  gainNode.connect(context.destination);
+  oscillator.start(0);
+  setTimeout(() => oscillator.stop(), duration);
+};
+
+const String = ({ freqs }: StringProps) => {
+  return (
+    <div className={styles.string}>
+      {freqs.map((freq) => (
+        <span className={styles.fret} onMouseDown={() => play(freq, 300)}>
+          {freq}
+        </span>
+      ))}
+    </div>
+  );
+};
 
 function App() {
+  const { E1, B2, G3, D4, A5, E6 } = freq;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <String freqs={E1} />
+      <String freqs={B2} />
+      <String freqs={G3} />
+      <String freqs={D4} />
+      <String freqs={A5} />
+      <String freqs={E6} />
     </div>
   );
 }
